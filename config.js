851d2fa4eb2b30e3eadce53db21949d12438a8d7
hung6033 <90978454+hung6033@users.noreply.github.com>
2023-08-29 23:21:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Tặng Chang',
-    introDesc: `Em có biết em khác những vì sao ở điểm nào không? Sao ở trên trời, em ở trong tim anh.
+    introTitle: 'Dear',
+    introDesc: `Tớ biết tụi mình đã có những cãi vã, sai lầm... Nhưng cậu có thể mở lòng, tha thứ cho tớ một lần được không?.
 `,
-    btnIntro: ':>',
-    title: 'Em làm nguoi yeu anh nhe',
-    desc: 'Hãy mở lòng và tin tưởng anh ',
-    btnYes: 'Okay :>',
-    btnNo: 'Không :<',
-    question:'Em sẽ ở bên cạnh anh chứ :<',
+    btnIntro: '...',
+    title: 'Không một giọt nước mắt nào có thể ngăn một người không muốn ở lại cả...',
+    desc: 'Nhưng dù vậy tớ cũng không muốn tụi mình làm kẻ thù của nhau... ',
+    btnYes: 'Vậy nên hãy nói chuyện với tớ nhé...',
+    btnNo: 'Không',
+    question:'Tớ sẽ không làm cậu tổn thương hay làm cậu phải khó chịu thêm lần nào nữa đâu',
     btnReply: 'Gửi',
-    reply: 'Đương nhiên rồiiii :>',
-    mess: 'Yeu emmm',
-    messDesc: 'Em rep tin nhắn  nhé :<',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Được rồi...',
+    mess: 'Sẽ không còn cãi vã nữa đâu',
+    messDesc: 'Gỡ block và nói chuyện với tớ nhé?',
+    btnAccept: '...',
     messLink: 'https://www.facebook.com/messages/t/100051992931764' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
