4f0eb603fe8da1a98be6a7f0a1a1af27b7ffdd8f
hung6033 <90978454+hung6033@users.noreply.github.com>
2024-02-27 22:22:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Dear',
-    introDesc: `Tớ biết tụi mình đã có những cãi vã, sai lầm... Nhưng cậu có thể mở lòng, tha thứ cho tớ một lần được không?.
+    introDesc: `Tớ biết tụi mình đã có những cãi vã, sai lầm... Nhưng cậu có thể mở lòng, tha thứ cho tớ một lần được không?
 `,
     btnIntro: '...',
     title: 'Không một giọt nước mắt nào có thể ngăn một người không muốn ở lại cả...',
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Gửi',
     reply: 'Được rồi...',
     mess: 'Sẽ không còn cãi vã nữa đâu',
-    messDesc: 'Gỡ block và nói chuyện với tớ nhé?',
+    messDesc: 'I Miss You',
     btnAccept: '...',
     messLink: 'https://www.facebook.com/messages/t/100051992931764' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
